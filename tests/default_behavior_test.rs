082ff193455f3bf89e5f15ff76602570d33c3133
Lennard Kittner <lennardkittner@icloud.com>
2023-07-22 02:25:19 +0200

    Added tests for defaults.

--- tests/default_behavior_test.rs
+++ tests/default_behavior_test.rs
@@ -1,67 +1,41 @@
 use std::process::{Termination, ExitCode};
-use std::fmt::Display;
 
-use thistermination::Termination;
+use thistermination::TerminationFull;
 
-#[derive(Termination)]
+#[derive(TerminationFull)]
+#[termination(exit_code(6), msg("test123 {}", 9 + 8))]
 enum Test {
-    UnitA,
-    UnitB(),
     #[termination(exit_code(7))]
-    UnitC,
+    UnitA,
     #[termination(msg("test"))]
-    UnitD,
     UnnamedA(u8),
     NamedA{x: u32},
-}
-
-impl Display for Test {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Test::UnitA => write!(f, "unit a"),
-            Test::UnitB() => write!(f, "unit b"),
-            Test::UnitC => write!(f, "unit c"),
-            Test::UnitD => write!(f, "unit d"),
-            Test::UnnamedA(field) => write!(f, "unnamed {field}"),
-            Test::NamedA { x } => write!(f, "named {}", x),
-        }
-    }
 }
 
 fn assert_eq_exit_code_and_int(ex: ExitCode, code: i32) {
     assert_eq!(format!("{:?}", ex), format!("ExitCode(unix_exit_status({}))", code));
 }
 
-fn assert_eq_exit_code_and_exit_code(ex1: ExitCode, ex2: ExitCode) {
-    assert_eq!(format!("{:?}", ex1), format!("{:?}", ex2));
-}
-
 #[test]
 fn unit_default() {
-    let unit_a = Test::UnitA;
-    let unit_b = Test::UnitB();
-    let unit_c = Test::UnitC;
-    let unit_d = Test::UnitD;
-    assert_eq!(format!("{:?}", unit_a), "unit a");
-    assert_eq!(format!("{:?}", unit_b), "unit b");
-    assert_eq!(format!("{:?}", unit_c), "unit c");
-    assert_eq!(format!("{:?}", unit_d), "test");
-    assert_eq_exit_code_and_exit_code(unit_a.report(), ExitCode::FAILURE);
-    assert_eq_exit_code_and_exit_code(unit_b.report(), ExitCode::FAILURE);
-    assert_eq_exit_code_and_int(unit_c.report(), 7);
-    assert_eq_exit_code_and_exit_code(unit_d.report(), ExitCode::FAILURE);
+    let unit = Test::UnitA;
+    assert_eq!(format!("{:?}", unit), "test123 17");
+    assert_eq!(format!("{}", unit), "test123 17");
+    assert_eq_exit_code_and_int(unit.report(), 7);
 }
 
 #[test]
 fn unnamed_default() {
     let unnamed = Test::UnnamedA(42);
-    assert_eq!(format!("{:?}", unnamed), "unnamed 42");
-    assert_eq_exit_code_and_exit_code(unnamed.report(), ExitCode::FAILURE);
+    assert_eq!(format!("{:?}", unnamed), "test");
+    assert_eq!(format!("{}", unnamed), "test");
+    assert_eq_exit_code_and_int(unnamed.report(), 6);
 }
 
 #[test]
 fn named_default() {
     let named = Test::NamedA{x: 1337};
-    assert_eq!(format!("{:?}", named), "named 1337");
-    assert_eq_exit_code_and_exit_code(named.report(), ExitCode::FAILURE);
+    assert_eq!(format!("{:?}", named), "test123 17");
+    assert_eq!(format!("{}", named), "test123 17");
+    assert_eq_exit_code_and_int(named.report(), 6);
 }
